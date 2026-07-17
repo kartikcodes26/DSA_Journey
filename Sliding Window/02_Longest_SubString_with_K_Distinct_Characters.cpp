c084321d2a9e3cfc8eace89e6e09015d4dc9d429
Kartik <kartikcodes26@gmail.com>
2026-07-17 23:51:09 +0530

    Added more programs

---
+++ Sliding Window/02_Longest_SubString_with_K_Distinct_Characters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+
+        unordered_map<char, int> mp;  // Create a hashmap
+        int low = 0;
+        int high = 0;
+        int res = 0;
+        // k a r t i k
+
+        for(high = 0; high < s.size(); high++)
+        {
+            mp[s[high]]++; // Add the element or increment it;
+            // mp.size() returns the number of distict elemets in it
+            while(mp.size() > k)
+            {
+                mp[s[low]]--;
+                if(mp[s[low]] == 0)
+                {
+                    mp.erase(s[low]);
+                }
+                low++;
+            }
+
+            if(mp.size() == k)
+            {
+                res = max(res, high - low + 1); // Return the size of the window
+            }
+        }
+
+        if(res == 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return res;
+        }
+
+    }
+};
